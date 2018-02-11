84055222404e03c2d5c76fd6abedca74983a5e28
殷正帅 <yinzhsh@fenqi.im>
2018-02-11 19:01:44 +0800

    增加数据动态绑定

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
 var app = angular.module("doudou", []);
 
-app.controller('imgList', function($scope, $http) {
+app.controller('imgList', function ($scope, $http) {
     $http.get("mock/imgList.json")
-         .then(function(response) {
+         .then(function (response) {
              $scope.imgList = response.data;
+             $scope.current = response.data[0];
+             $scope.current.active = true;
          });
+    $scope.change = function ($item) {
+        $scope.current = $item;
+
+        angular.forEach($scope.imgList, function (value, key) {
+            value.active = false;
+        });
+
+        $scope.current.active = true;
+    };
 });
+
